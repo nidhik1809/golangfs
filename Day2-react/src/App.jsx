9b8d0cce3fcdddb8c411734e0bcef1acba8b0762
nidhik1809 <nidhik.engg@gmail.com>
2025-02-13 10:06:37 +0530

    Day 2 level 2 design complete

---
+++ Day2-react/src/App.jsx
@@ -0,0 +1,24 @@
+import CarView from "./cars/CarView"
+import CarList from "./cars/CarList"
+import CarCreate from "./cars/CarCreate"
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path="" element={<CarList/>}/>
+            <Route path="/car/list" element={<CarList/>}/>
+            <Route path="/car/create" element={<CarCreate/>}/>
+            <Route path="/car/view" element={<CarView/>}/>
+            <Route path="car/car/view" element={<CarView/>}/>
+            <Route path="car/car/create" element={<CarCreate/>}/>
+        </Routes>
+      
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
